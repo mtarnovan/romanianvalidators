ae7b77dda5bb2ca5b212659d784a660169859605
Mihai Târnovan <mihai.tarnovan@cubus.ro>
2013-04-28 14:53:36 +0300

    Refactoring

--- lib/active_model/validations/cnp_validator.rb
+++ lib/active_model/validations/cnp_validator.rb
@@ -2,6 +2,8 @@ module ActiveModel
   module Validations
     class CnpValidator < EachValidator
 
+      TEST_KEY = "279146358279"
+
       include ActiveModel::Validations::EmptyBlankEachValidator
 
       # Algoritm de validare CNP
@@ -17,28 +19,36 @@ module ActiveModel
       # reprezinta cifra de control. Pentru un CNP valid acest rest va trebui sa coincida cu cifra de pe
       # pozitia treisprezece din CNP-ul initial.
       def valid?(cnp)
-        return false unless (cnp=~(/[^0-9]/)).nil? && cnp.size == 13
-        begin
-          year = case
-          when
-            cnp[0].chr == "1" || cnp[0].chr == "2" then "19"
-          when
-            cnp[0].chr == "3" || cnp[0].chr == "4" then "18"
-          when
-            cnp[0].chr == "5" || cnp[0].chr == "6" then "20"
-          when
-            cnp[0].chr == "9" then "19" # oare se sare peste un an bisect intre 1800-2099 ?
-          else return false;
-          end
+        return false unless well_formed?(cnp) && valid_birthdate?(cnp)
 
-          year = (year + cnp[1..2]).to_i
-          return false unless Date.valid_civil?(year,cnp[3..4].to_i,cnp[5..6].to_i)
-        rescue ArgumentError
-          return false
-        end
-        test_key = "279146358279"
-         (0..11).inject(0){|sum, n| sum += test_key[n].chr.to_i * cnp[n].chr.to_i} % 11 == cnp[12].chr.to_i
+        (0..11).inject(0){|sum, n| sum += TEST_KEY[n].chr.to_i * cnp[n].chr.to_i} % 11 == cnp[12].chr.to_i
       end
+
+      private
+
+        def well_formed?(cnp)
+          (cnp=~(/[^0-9]/)).nil? && cnp.size == 13
+        end
+
+        def valid_birthdate?(cnp)
+          begin
+            year = case
+            when
+              cnp[0].chr == "1" || cnp[0].chr == "2" then "19"
+            when
+              cnp[0].chr == "3" || cnp[0].chr == "4" then "18"
+            when
+              cnp[0].chr == "5" || cnp[0].chr == "6" then "20"
+            when
+              cnp[0].chr == "9" then "19" # oare se sare peste un an bisect intre 1800-2099 ?
+            else return false;
+            end
+            year = (year + cnp[1..2]).to_i
+            Date.valid_civil?(year, cnp[3..4].to_i, cnp[5..6].to_i) ? true : false
+          rescue ArgumentError
+            return false
+          end
+        end
 
     end
   end
