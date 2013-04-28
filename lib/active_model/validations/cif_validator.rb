18391986e2c7d8923ef5f82f2963ab96303a508b
Mihai Târnovan <mihai.tarnovan@cubus.ro>
2013-04-28 14:42:30 +0300

    Refactoring

--- lib/active_model/validations/cif_validator.rb
+++ lib/active_model/validations/cif_validator.rb
@@ -2,6 +2,9 @@ module ActiveModel
   module Validations
     class CifValidator < EachValidator
 
+      # reversed test key (753217532)
+      TEST_KEY = "235712357".freeze
+
       include ActiveModel::Validations::EmptyBlankEachValidator
 
       # Algoritmul de validare al unui cod CUI
@@ -11,13 +14,16 @@ module ActiveModel
       # Pas 3: Se aduna toate produsele obtinute. Suma rezultata se inmulteste cu 10 si apoi se afla restul impartirii la 11.
       # Pas 4: Pentru un CUI valid cifra obtinuta, in urma operatiei MODULO 11, va trebui sa corespunda cu cifra de control a codului CUI initial .
       def valid?(cif)
-        return false if cif.nil?
-        return false if cif.size > 10 || cif.size < 2
-        return false unless (cif=~(/[^0-9]/)).nil?
-        rk = "235712357".freeze # reversed test key (753217532)
+        return false unless well_formed?(cif)
         rc = cif.reverse.freeze
-        (1..(cif.size - 1)).inject(0) {|sum, n| sum += rk[n-1].chr.to_i * rc[n].chr.to_i}  * 10 % 11 % 10 == rc[0].chr.to_i
+        (1..(cif.size - 1)).inject(0) {|sum, n| sum += TEST_KEY[n-1].chr.to_i * rc[n].chr.to_i}  * 10 % 11 % 10 == rc[0].chr.to_i
       end
+
+      private
+
+        def well_formed?(cif)
+          cif.present? && (2..10).include?(cif.size) && (cif=~(/[^0-9]/)).nil?
+        end
 
     end
   end
